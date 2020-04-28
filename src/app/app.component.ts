26ace2407f116b21432b5ed5590604cd49bfc54d
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2020-04-28 11:48:53 +0300

    fixed tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,28 +19,34 @@ interface MetalData {
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-  public lineChartData: ChartDataSets[] = [
+  goldData: ChartDataSets[] = [
     { data: []},
   ];
-  public lineChartLabels: Label[] = [];
-  public lineChartOptions: ChartOptions = {
+  goldLabels: Label[] = [];
+  silverData: ChartDataSets[] = [
+    { data: []},
+  ];
+  silverLabels: Label[] = [];
+  lineChartOptions: ChartOptions = {
     responsive: true,
     maintainAspectRatio: false
   };
-  public lineChartColors: Color[] = [
+  lineChartColors: Color[] = [
     {
       borderColor: 'black',
       backgroundColor: 'rgb(239,208,111)',
     },
   ];
-  public lineChartType = 'line';
+  lineChartType = 'line';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get(`${environment.baseUrl}/get-data`).subscribe((data: MetalData) => {
-      this.lineChartData[0].data = data.gold.map(dataPoint => dataPoint.price);
-      this.lineChartLabels = data.gold.map(dataPoint => dataPoint.year);
+      this.goldData[0].data = data.gold.map(dataPoint => dataPoint.price);
+      this.silverData[0].data = data.silver.map(dataPoint => dataPoint.price);
+      this.goldLabels = data.gold.map(dataPoint => dataPoint.year);
+      this.silverLabels = data.silver.map(dataPoint => dataPoint.year);
     })
   }
 }
